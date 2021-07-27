ea2cf9768551104913602a416c7f1e36ec8cb10e
Ilya Gordey <wdybelievein@gmail.com>
2021-07-27 13:50:09 +0300

    Add package shape

---
+++ cmd/hw_04/go.mod
@@ -0,0 +1,7 @@
+module golang-course/cmd/hw_04
+
+go 1.16
+
+replace golang-course/cmd/hw_04/shape => /shape
+
+require golang-course/cmd/hw_04/shape v0.0.0-00010101000000-000000000000 // indirect
--- cmd/hw_04/main.go
+++ cmd/hw_04/main.go
@@ -2,58 +2,18 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"golang-course/cmd/hw_04/shape"
 )
 
-type Shape interface {
-	Area() float64
-	Perimeter() float64
-}
-
-type Circle struct {
-	radius float64
-}
-
-type Rectangle struct {
-	width float64
-	height float64
-}
-
-func (x Circle) Area() float64 {
-	return math.Pi * x.radius * x.radius
-}
-
-func (x Circle) Perimeter() float64 {
-	return 2 * math.Pi * x.radius
-}
-
-func (x Rectangle) Area() float64 {
-	return x.width * x.height
-}
-
-func (x Rectangle) Perimeter() float64 {
-	return (x.width + x.height) * 2
-}
-
-func (x Circle) String() string {
-	return fmt.Sprintf("Circle: radius %.2f", x.radius)
-}
-
-func (x Rectangle) String() string {
-	return fmt.Sprintf("Rectangle with height %.2f and width %.2f", x.height, x.width)
-}
-
-func DescribeShape(s Shape) {
+func DescribeShape(s shape.Shape) {
 	fmt.Println(s)
 	fmt.Printf("Area: %.2f\n", s.Area())
 	fmt.Printf("Perimeter: %.2f\n", s.Perimeter())
 }
+
 func main() {
-	c := Circle{8}
-	r := Rectangle{
-		height: 9,
-		width:  3,
-	}
+	c := shape.NewCircle(8)
+	r := shape.NewRectangle(9, 3)
 	DescribeShape(c)
 	DescribeShape(r)
 }
