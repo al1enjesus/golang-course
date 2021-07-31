19f13cf910ff2ba4f90c64995d9425c944998d4b
Ilya Gordey <wdybelievein@gmail.com>
2021-07-31 22:20:18 +0300

    Add task_02

---
+++ cmd/hw_06/task_02/client.go
@@ -0,0 +1,23 @@
+package main
+
+
+import "net"
+import "fmt"
+import "bufio"
+import "os"
+
+func main() {
+	conn, _ := net.Dial("tcp", "127.0.0.1:8081")
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Text to send: ")
+		text, _ := reader.ReadString('\n')
+
+		fmt.Fprintf(conn, text + "\n")
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+		if message == "EXIT\n" {
+			os.Exit(0)
+		}
+		fmt.Print("Message from server: "+message)
+	}
+}
